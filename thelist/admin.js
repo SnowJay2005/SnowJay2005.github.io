1dfd28b45a729d557f92451890dcbb94ba6be282
SnowJay2005 <98613460+SnowJay2005@users.noreply.github.com>
2023-07-02 01:16:52 +0800

    VersionsMe1

--- thelist/admin.js
+++ thelist/admin.js
@@ -16,71 +16,72 @@ var itemListElement = document.getElementById('item-list');
 var totalElement = document.getElementById('total');
 
 // Function to display the item list
-function displayItemList(items) {
-  itemListElement.innerHTML = '';
-  totalElement.textContent = items.length;
+function displayItemList() {
+  db.ref('items').on('value', function (snapshot) {
+    itemListElement.innerHTML = '';
 
-  items.forEach(function (item) {
-    var itemCard = document.createElement('div');
-    itemCard.classList.add('item-card');
+    try {
+      snapshot.forEach(function (childSnapshot) {
+        var item = childSnapshot.val();
+        var itemKey = childSnapshot.key; // Retrieve the key of the item
 
-    var itemName = document.createElement('a');
-    itemName.textContent = item.name;
-    itemName.href = 'item.html#' + encodeURIComponent(item.name);
+        var itemCard = document.createElement('div');
+        itemCard.classList.add('item-card');
 
-    var itemCount = document.createElement('p');
-    itemCount.textContent = 'Count: ' + item.count;
+        var itemName = document.createElement('a');
+        itemName.textContent = item.name;
+        itemName.href = 'item.html#' + encodeURIComponent(item.name);
 
-    var itemVersion = document.createElement('p');
-    itemVersion.textContent = 'Version: ' + item.version;
+        var itemVersion = document.createElement('a');
+        itemVersion.textContent = item.version;
 
-    var itemImage = document.createElement('img');
-    itemImage.src = item.image;
-    itemImage.alt = item.name;
+        var itemCount = document.createElement('p');
+        itemCount.textContent = 'Count: ' + item.count;
 
-    var itemDescription = document.createElement('p');
-    itemDescription.textContent = 'Description: ' + item.description;
+        var editButton = document.createElement('button');
+        editButton.textContent = 'Edit';
+        editButton.addEventListener('click', function () {
+          editItem(itemKey, item); // Pass the key and item to the edit function
+        });
 
-    var editButton = document.createElement('button');
-    editButton.textContent = 'Edit';
-    editButton.addEventListener('click', function () {
-      editItem(item.key, item); // Pass the key and item to the edit function
-    });
+        var deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', function () {
+          deleteItem(itemKey); // Pass the key to the delete function
+        });
 
-    var deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.addEventListener('click', function () {
-      deleteItem(item.key); // Pass the key to the delete function
-    });
+        itemCard.appendChild(itemName);
+        itemCard.appendChild(itemVersion);
+        itemCard.appendChild(itemCount);
+        itemCard.appendChild(editButton);
+        itemCard.appendChild(deleteButton);
+        itemListElement.appendChild(itemCard);
+      });
 
-    itemCard.appendChild(itemName);
-    itemCard.appendChild(itemCount);
-    itemCard.appendChild(itemVersion);
-    itemCard.appendChild(itemImage);
-    itemCard.appendChild(itemDescription);
-    itemCard.appendChild(editButton);
-    itemCard.appendChild(deleteButton);
-    itemListElement.appendChild(itemCard);
+      totalElement.textContent = snapshot.numChildren();
+    } catch (e) {
+      console.error(e);
+    }
   });
 }
 
 // Add an event listener for the form submission
 var newItemForm = document.getElementById('new-item-form');
-newItemForm.addEventListener('submit', function (event) {
+newItemForm.addEventListener('submit', function(event) {
   event.preventDefault(); // Prevent the form from submitting and refreshing the page
 
   // Get the input values
   var itemName = document.getElementById('item-name').value;
-  var itemCount = parseInt(document.getElementById('item-count').value);
   var itemVersion = document.getElementById('item-version').value;
+  var itemCount = parseInt(document.getElementById('item-count').value);
   var itemImage = document.getElementById('item-image').value;
   var itemDescription = document.getElementById('item-description').value;
 
   // Create a new item object
   var newItem = {
     name: itemName,
-    count: itemCount,
     version: itemVersion,
+    count: itemCount,
     image: itemImage,
     description: itemDescription
   };
@@ -88,22 +89,21 @@ newItemForm.addEventListener('submit', function (event) {
   // Save the new item to Firebase
   var newItemRef = db.ref('items').push();
   newItemRef.set(newItem)
-    .then(function () {
+    .then(function() {
       console.log('New item added successfully');
       newItemForm.reset(); // Reset the form fields
     })
-    .catch(function (error) {
+    .catch(function(error) {
       console.error('Error adding new item:', error);
     });
 });
 
 // Listen for changes in the items data and update the item list
-db.ref('items').on('value', function (snapshot) {
+db.ref('items').on('value', function(snapshot) {
   var items = [];
 
-  snapshot.forEach(function (childSnapshot) {
+  snapshot.forEach(function(childSnapshot) {
     var item = childSnapshot.val();
-    item.key = childSnapshot.key; // Add the key to the item object
     items.push(item);
   });
 
@@ -111,7 +111,7 @@ db.ref('items').on('value', function (snapshot) {
 });
 
 // Load the item content based on the URL hash
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', function() {
   var itemHash = window.location.hash.substr(1);
   var itemName = decodeURIComponent(itemHash);
 
@@ -120,7 +120,7 @@ window.addEventListener('DOMContentLoaded', function () {
     .equalTo(itemName)
     .limitToFirst(1)
     .once('value')
-    .then(function (snapshot) {
+    .then(function(snapshot) {
       if (!snapshot.exists()) {
         // Handle invalid or non-existing item names
         console.log('Item not found: ' + itemName);
@@ -129,30 +129,43 @@ window.addEventListener('DOMContentLoaded', function () {
         updateItemPage(item);
       }
     })
-    .catch(function (error) {
+    .catch(function(error) {
       console.log('Error getting item:', error);
     });
+});
+
+// Call the displayItemList function to populate the item list on the index.html page
+db.ref('items').once('value', function(snapshot) {
+  var items = [];
+
+  snapshot.forEach(function(childSnapshot) {
+    var item = childSnapshot.val();
+    items.push(item);
+  });
+
+  displayItemList(items);
 });
 
 // Function to handle editing an item
 function editItem(key, item) {
   var newName = prompt('Enter a new name:', item.name);
-  var newCount = parseInt(prompt('Enter a new count:', item.count), 10);
   var newVersion = prompt('Enter a new version:', item.version);
+  var newCount = parseInt(prompt('Enter a new count:', item.count), 10);
   var newImage = prompt('Enter a new image URL:', item.image);
   var newDescription = prompt('Enter a new description:', item.description);
 
-  if (newName && !isNaN(newCount) && newVersion && newImage && newDescription) {
+  if (newName && !isNaN(newCount) && newImage && newDescription) {
     var updates = {};
     updates['items/' + key + '/name'] = newName;
-    updates['items/' + key + '/count'] = newCount;
     updates['items/' + key + '/version'] = newVersion;
+    updates['items/' + key + '/count'] = newCount;
     updates['items/' + key + '/image'] = newImage;
     updates['items/' + key + '/description'] = newDescription;
 
     db.ref().update(updates);
   }
 }
+
 
 // Function to handle deleting an item
 function deleteItem(key) {
