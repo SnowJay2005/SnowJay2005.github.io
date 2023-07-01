e5fb47b919c7f7295c27bc48bab19876f883f30d
SnowJay2005 <98613460+SnowJay2005@users.noreply.github.com>
2023-07-01 21:14:53 +0800

    Firebase 20

--- thelist/firebase.js
+++ thelist/firebase.js
@@ -1,3 +1,7 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
 // Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDPKjpimOZLKwNwfm_IQFm8X4Pv2ZgucIA",
