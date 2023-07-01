c0a9ee80a3f533dc580c900b5dca89f2b909d7d8
SnowJay2005 <98613460+SnowJay2005@users.noreply.github.com>
2023-07-01 18:49:51 +0800

    Editing 1

--- thelist/script.js
+++ thelist/script.js
@@ -7,7 +7,7 @@ var Item = function(name, count, image, description) {
 };
 
 // Define the itemList array with item objects
-var itemList = [
+var itemList = getItemListFromLocalStorage() || [
   new Item('Realistic Sans', 25, 'path/to/realistic-sans-image.jpg', 'Description of Realistic Sans'),
   new Item('Barbarian', 18, 'path/to/barbarian-image.jpg', 'Description of Barbarian')
   // Add other items here
@@ -33,8 +33,18 @@ function displayItemList() {
     var itemCount = document.createElement('p');
     itemCount.textContent = 'Count: ' + item.count;
 
+    var editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.addEventListener('click', createEditItemHandler(item));
+
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', createDeleteItemHandler(item));
+
     itemCard.appendChild(itemName);
     itemCard.appendChild(itemCount);
+    itemCard.appendChild(editButton);
+    itemCard.appendChild(deleteButton);
     itemListElement.appendChild(itemCard);
   }
 }
@@ -49,17 +59,29 @@ function findItemByName(name) {
   return null;
 }
 
-// Update the item page with the content of the selected item
-function updateItemPage(item) {
-  var itemNameElement = document.getElementById('item-name');
-  var itemCountElement = document.getElementById('item-count');
-  var itemImageElement = document.getElementById('item-image');
-  var itemDescriptionElement = document.getElementById('item-description');
+// Create a click event handler for editing an item
+function createEditItemHandler(item) {
+  return function () {
+    // Redirect to the edit.html page with the item's details pre-filled
+    window.location.href = 'edit.html#' + encodeURIComponent(item.name);
+  };
+}
 
-  itemNameElement.textContent = item.name;
-  itemCountElement.textContent = 'Count: ' + item.count.toString();
-  itemImageElement.src = item.image;
-  itemDescriptionElement.textContent = item.description;
+// Create a click event handler for deleting an item
+function createDeleteItemHandler(item) {
+  return function () {
+    // Remove the item from the itemList
+    var itemIndex = itemList.indexOf(item);
+    if (itemIndex !== -1) {
+      itemList.splice(itemIndex, 1);
+    }
+
+    // Save the updated itemList to local storage
+    saveItemList();
+
+    // Refresh the item list on the index.html page
+    displayItemList();
+  };
 }
 
 // Load the item content based on the URL hash
@@ -76,5 +98,21 @@ function loadItemContent() {
   }
 }
 
+// Save the itemList array to local storage
+function saveItemList() {
+  localStorage.setItem('itemList', JSON.stringify(itemList));
+}
+
+// Retrieve the itemList array from local storage
+function getItemListFromLocalStorage() {
+  var itemListJSON = localStorage.getItem('itemList');
+  return itemListJSON ? JSON.parse(itemListJSON) : null;
+}
+
 // Call the displayItemList function to populate the item list on the index.html page
 displayItemList();
+
+// Check if the page is an item page and load the item content if necessary
+if (window.location.pathname.includes('/thelist/item.html')) {
+  loadItemContent();
+}
