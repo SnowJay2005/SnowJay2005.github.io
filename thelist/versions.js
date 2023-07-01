6d795b6870b1c5216d9c791d161d1a7293686393
SnowJay2005 <98613460+SnowJay2005@users.noreply.github.com>
2023-07-02 02:56:46 +0800

    versions.html 2

--- thelist/versions.js
+++ thelist/versions.js
@@ -1,46 +1,46 @@
-// Initialize Firebase
-var config = {
-    // Your Firebase configuration
-  };
-  
-  firebase.initializeApp(config);
-  
-  // Reference to the Firebase database
-  var db = firebase.database();
-  
-  // Get the item name from the URL hash
-  var itemName = decodeURIComponent(window.location.hash.substr(1));
-  
-  // Reference to the versions of the item in the database
-  var versionsRef = db.ref('items/' + itemName);
-  
-  // Function to display the versions of the item
-  function displayVersions() {
-    versionsRef.on('value', function(snapshot) {
-      var versionsList = document.getElementById('versionsList');
-      versionsList.innerHTML = '';
-  
-      snapshot.forEach(function(childSnapshot) {
-        var version = childSnapshot.key;
-        var versionData = childSnapshot.val();
-  
+// Retrieve the item name from the URL
+var itemName = decodeURIComponent(window.location.hash.substr(1));
+
+// Get a reference to the versions of the selected item
+var versionsRef = db.ref('items/' + itemName);
+
+// Function to display the versions of the selected item
+function displayVersions() {
+  versionsRef.on('value', function (snapshot) {
+    versionsListElement.innerHTML = '';
+
+    try {
+      snapshot.forEach(function (childSnapshot) {
+        var version = childSnapshot.key; // Get the version key
+        var versionData = childSnapshot.val(); // Get the version data
+
         var versionCard = document.createElement('div');
         versionCard.classList.add('version-card');
-  
+
         var versionName = document.createElement('h2');
         versionName.textContent = version;
-  
+
         var versionCount = document.createElement('p');
         versionCount.textContent = 'Count: ' + versionData.count;
-  
-        // You can add more elements to display other properties of the version
-  
+
+        var versionDescription = document.createElement('p');
+        versionDescription.textContent = 'Description: ' + versionData.description;
+
+        var versionImage = document.createElement('img');
+        versionImage.src = versionData.image;
+        versionImage.alt = version;
+
         versionCard.appendChild(versionName);
         versionCard.appendChild(versionCount);
-        versionsList.appendChild(versionCard);
+        versionCard.appendChild(versionDescription);
+        versionCard.appendChild(versionImage);
+        versionsListElement.appendChild(versionCard);
       });
-    });
-  }
-  
-  // Call the displayVersions function to populate the versions list
-  displayVersions();
+    } catch (e) {
+      console.error(e);
+    }
+  });
+}
+
+// Call the function to display the versions
+displayVersions();
