f1fd1e30574f389c7ae51f4efbce10fd1d39ef1e
SnowJay2005 <98613460+SnowJay2005@users.noreply.github.com>
2023-07-01 21:18:31 +0800

    Firebase 21

--- thelist/script.js
+++ thelist/script.js
@@ -73,6 +73,25 @@ function loadItemContent() {
     });
 }
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDPKjpimOZLKwNwfm_IQFm8X4Pv2ZgucIA",
+    authDomain: "the-list-175d3.firebaseapp.com",
+    databaseURL: "https://the-list-175d3-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "the-list-175d3",
+    storageBucket: "the-list-175d3.appspot.com",
+    messagingSenderId: "133562798111",
+    appId: "1:133562798111:web:34a18cd64c6202314cc52a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
 // Call the displayItemList function to populate the item list on the index.html page
 displayItemList();
 
