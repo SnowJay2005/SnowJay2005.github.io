2b6098a53b3608d4d70ba6b584748a291df1999d
SnowJay2005 <98613460+SnowJay2005@users.noreply.github.com>
2023-07-02 03:12:32 +0800

    versions.html 8

--- thelist/versions.js
+++ thelist/versions.js
@@ -9,53 +9,56 @@ var firebaseConfig = {
     appId: "1:133562798111:web:34a18cd64c6202314cc52a"
     };
   
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  // Function to display the versions
-  function displayVersions() {
-    // Retrieve the item name from the URL hash
-    var itemName = decodeURIComponent(location.hash.substr(1));
-  
-    // Set the item name in the header
-    document.getElementById('item-name').textContent = itemName;
-  
-    // Get a reference to the versions of the item
-    var versionsRef = db.ref('items/' + itemName);
-  
-    // Attach a listener to listen for changes in the versions
-    versionsRef.on('value', function(snapshot) {
-      var versionsListElement = document.getElementById('versions-list');
-      versionsListElement.innerHTML = '';
-  
-      snapshot.forEach(function(childSnapshot) {
-        var version = childSnapshot.key;
-        var versionData = childSnapshot.val();
-  
-        var versionCard = document.createElement('div');
-        versionCard.classList.add('item-card');
-  
-        var versionName = document.createElement('h2');
-        versionName.textContent = 'Version ' + version;
-  
-        var versionCount = document.createElement('p');
-        versionCount.textContent = 'Count: ' + versionData.count;
-  
-        var versionDescription = document.createElement('p');
-        versionDescription.textContent = 'Description: ' + versionData.description;
-  
-        var versionImage = document.createElement('img');
-        versionImage.src = versionData.image;
-        versionImage.alt = 'Version ' + version + ' Image';
-  
-        versionCard.appendChild(versionName);
-        versionCard.appendChild(versionCount);
-        versionCard.appendChild(versionDescription);
-        versionCard.appendChild(versionImage);
-        versionsListElement.appendChild(versionCard);
-      });
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the Firebase Realtime Database
+var db = firebase.database();
+
+// Function to display the versions
+function displayVersions() {
+  // Retrieve the item name from the URL hash
+  var itemName = decodeURIComponent(location.hash.substr(1));
+
+  // Set the item name in the header
+  document.getElementById('item-name').textContent = itemName;
+
+  // Get a reference to the versions of the item
+  var versionsRef = db.ref('items/' + itemName);
+
+  // Attach a listener to listen for changes in the versions
+  versionsRef.on('value', function(snapshot) {
+    var versionsListElement = document.getElementById('versions-list');
+    versionsListElement.innerHTML = '';
+
+    snapshot.forEach(function(childSnapshot) {
+      var version = childSnapshot.key;
+      var versionData = childSnapshot.val();
+
+      var versionCard = document.createElement('div');
+      versionCard.classList.add('item-card');
+
+      var versionName = document.createElement('h2');
+      versionName.textContent = 'Version ' + version;
+
+      var versionCount = document.createElement('p');
+      versionCount.textContent = 'Count: ' + versionData.count;
+
+      var versionDescription = document.createElement('p');
+      versionDescription.textContent = 'Description: ' + versionData.description;
+
+      var versionImage = document.createElement('img');
+      versionImage.src = versionData.image;
+      versionImage.alt = 'Version ' + version + ' Image';
+
+      versionCard.appendChild(versionName);
+      versionCard.appendChild(versionCount);
+      versionCard.appendChild(versionDescription);
+      versionCard.appendChild(versionImage);
+      versionsListElement.appendChild(versionCard);
     });
-  }
-  
-  // Call the displayVersions function when the page loads
-  window.onload = displayVersions;
+  });
+}
+
+// Call the displayVersions function when the page loads
+window.onload = displayVersions;
