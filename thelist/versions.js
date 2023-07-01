d6f4a3394a85ef582e10cab9f78f0eec3313edbd
SnowJay2005 <98613460+SnowJay2005@users.noreply.github.com>
2023-07-02 03:10:19 +0800

    versions.html 7

--- thelist/versions.js
+++ thelist/versions.js
@@ -1,43 +1,61 @@
-// Get the item name from the URL hash
-var itemName = decodeURIComponent(window.location.hash.substr(1));
-
-// Display the item name
-var itemNameElement = document.getElementById('item-name');
-itemNameElement.textContent = itemName;
-
-// Function to display the versions of an item
-function displayVersions() {
-  db.ref('items/' + itemName).on('value', function (snapshot) {
-    versionsListElement.innerHTML = '';
-
-    try {
-      snapshot.forEach(function (childSnapshot) {
-        var versionKey = childSnapshot.key; // Retrieve the key of the version
-        var version = childSnapshot.val();
-
+// Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDPKjpimOZLKwNwfm_IQFm8X4Pv2ZgucIA",
+    authDomain: "the-list-175d3.firebaseapp.com",
+    databaseURL: "https://the-list-175d3-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "the-list-175d3",
+    storageBucket: "the-list-175d3.appspot.com",
+    messagingSenderId: "133562798111",
+    appId: "1:133562798111:web:34a18cd64c6202314cc52a"
+    };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  // Function to display the versions
+  function displayVersions() {
+    // Retrieve the item name from the URL hash
+    var itemName = decodeURIComponent(location.hash.substr(1));
+  
+    // Set the item name in the header
+    document.getElementById('item-name').textContent = itemName;
+  
+    // Get a reference to the versions of the item
+    var versionsRef = db.ref('items/' + itemName);
+  
+    // Attach a listener to listen for changes in the versions
+    versionsRef.on('value', function(snapshot) {
+      var versionsListElement = document.getElementById('versions-list');
+      versionsListElement.innerHTML = '';
+  
+      snapshot.forEach(function(childSnapshot) {
+        var version = childSnapshot.key;
+        var versionData = childSnapshot.val();
+  
         var versionCard = document.createElement('div');
-        versionCard.classList.add('version-card');
-
+        versionCard.classList.add('item-card');
+  
+        var versionName = document.createElement('h2');
+        versionName.textContent = 'Version ' + version;
+  
         var versionCount = document.createElement('p');
-        versionCount.textContent = 'Count: ' + version.count;
-
-        var versionImage = document.createElement('img');
-        versionImage.src = version.image;
-        versionImage.alt = 'Version Image';
-
+        versionCount.textContent = 'Count: ' + versionData.count;
+  
         var versionDescription = document.createElement('p');
-        versionDescription.textContent = 'Description: ' + version.description;
-
+        versionDescription.textContent = 'Description: ' + versionData.description;
+  
+        var versionImage = document.createElement('img');
+        versionImage.src = versionData.image;
+        versionImage.alt = 'Version ' + version + ' Image';
+  
+        versionCard.appendChild(versionName);
         versionCard.appendChild(versionCount);
-        versionCard.appendChild(versionImage);
         versionCard.appendChild(versionDescription);
+        versionCard.appendChild(versionImage);
         versionsListElement.appendChild(versionCard);
       });
-    } catch (e) {
-      console.error(e);
-    }
-  });
-}
-
-// Call the displayVersions function to show the versions of the item
-displayVersions();
+    });
+  }
+  
+  // Call the displayVersions function when the page loads
+  window.onload = displayVersions;
