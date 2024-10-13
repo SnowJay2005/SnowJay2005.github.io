412cfff156a40195156eda20ac7e954342774aef
SnowJay2005 <jbsnowballspam@gmail.com>
2024-10-13 14:44:02 +0800

    Made guessfromaframe show score and be better

--- guessfromaframe/shows.js
+++ guessfromaframe/shows.js
@@ -1,8 +1,11 @@
-// List of shows and links to their respective games
+// List of shows, links to their respective games, and their high score keys
 const shows = [
-    { name: 'The Venture Bros.', link: '../venturebrosguesser/index.html' },
-	{ name: 'Gravity Falls', link: '../gravityfallsguesser/index.html' },
-	{ name: 'Twin Peaks', link: '../twinpeaksguesser/index.html' },
-	{ name: 'Saw', link: '../sawguesser/index.html' },
+    { name: 'The Venture Bros.', link: '../venturebrosguesser/index.html', highScoreKey: 'highScore_VentureBros' },
+    { name: 'Gravity Falls', link: '../gravityfallsguesser/index.html', highScoreKey: 'highScore_GravityFalls' },
+    { name: 'Twin Peaks', link: '../twinpeaksguesser/index.html', highScoreKey: 'highScore_TwinPeaks' },
+    { name: 'Saw', link: '../sawguesser/index.html', highScoreKey: 'highScore_Saw' },
     // Add more shows as needed
 ];
+
+// Sort shows alphabetically by name
+shows.sort((a, b) => a.name.localeCompare(b.name));
